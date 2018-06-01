fff6c3681e3ba360388fd4c08a3e886eb770e1e5
Nikita <nikita06012003@mail.ru>
2018-06-01 23:41:24 +0300

    2 урок 1 доп задание

--- lesson2.2/script.js
+++ lesson2.2/script.js
@@ -1,7 +1,7 @@
 var week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
 	 'Пятница', 'Суббота', 'Воскресенье']; 
 var date = new Date(Date.now());
-var wk = (date.getDay() - 1) % 7;
+var wk = (date.getDay() - 1) % 7; //Т.к. вс - 1 день недели, а в массиве - последний 
 
 for(let i = 0; i < 7; i++){
 	if(i < 5 && i != wk) {
@@ -13,4 +13,14 @@ for(let i = 0; i < 7; i++){
 		} else{
 			document.write("<strong>" + "<em>" + week[i] + "</em>" + "</strong>" + "<br>");
 		}
+}
+
+
+var arr = ['12345', '324523', '7542434', '53', '7135', '93253', '31854'];
+
+for(let i = 0; i < 7; i++){
+	let a = arr[i].charAt(0);
+	if(a == '3' || a == '7'){
+		console.log(arr[i]);
+	}
 }
