daba76e8befba2ef570f63ce332ac44fb537b0f8
Nikita <nikita06012003@mail.ru>
2018-06-06 13:57:34 +0300

    4 урок главное задание

--- mainLesson1/script.js
+++ mainLesson1/script.js
@@ -8,73 +8,97 @@ function start () {
  	while (isNaN(money) || money == "" || money == null){
 		money = prompt("Ваш бюджет на месяц?", 40000);
  	}
+ 	mainList.budget = money;
  	name = prompt("Название вашего магазина?", "Название").toUpperCase();
+ 	mainList.shopName = name;
  	time = 21;
  }
-
-start();
 
 let mainList = {
 	budget: money,
 	shopName: name,
 	employers: {},
 	shopGoods: [],
-	open: true,
-	discount: false
-}
+	open: false,
+	discount: false,
+	shopItems: [],
+	chooseGoods: function chooseGoods(){
+		for (let i = 0; i < 5; i++){
+			let a = prompt("Какой тип товаров будем продавать?", "");
 
-function employ() {
-	for (let i = 1; i <= 4; i++){
-		let a = prompt("Введите имя " + i + "-го сотрудника");
-		if(a != "" && a != null && isNaN(a))
-			mainList.employers[1] = a;
-	}
-}
-
-employ();
-
-function chooseGoods(){
-	for (let i = 0; i < 5; i++){
-		let a = prompt("Какой тип товаров будем продавать?");
-
-		if((typeof(a)) === 'string' && !(typeof(a) === null) && a!='' && a.length < 50){
-				mainList.shopGoods[i] = a;
-		} else {
-			console.log('Некорректный ввод!');
-			--i;
+			if((typeof(a)) === 'string' && !(typeof(a) === null) && a!='' && a.length < 50){
+					mainList.shopGoods[i] = a;
+			} else {
+				console.log('Некорректный ввод!');
+				--i;
+			}
 		}
+	},
+	chooseShopItems: function chooseShopItems() {
+		let items;
+		do{
+			items = prompt("Перечислите через запятую товары", "");
+
+			if((typeof(items)) != 'string' || typeof(items) === null || items == '')
+				items = null;
+			else{
+				console.log("Некорректный ввод");
+			}
+		} while (items == null);
+
+		mainList.shopItems = items.split(",");
+		mainList.shopItems.push(prompt("Подождите, еще ", ""));
+		mainList.shopItems.sort();
+	},
+	showUCanBuy: function showUCanBuy(){
+		let str = "У нас вы можете купить: ";
+		mainList.shopItems.forEach(function(item, i, arr){
+			str+= "\n" + (i+1) + ") " +	item;
+		})
+		alert(str);
+	}, 
+	hireEmployers: function hireEmployers() {
+		for (let i = 1; i <= 4; i++){
+			let a = prompt("Введите имя " + i + "-го сотрудника", "");
+			if(a != "" && a != null && isNaN(a))
+				mainList.employers[1] = a;
+		}
+	},
+	workTime: function workTime(time){
+		if(time < 0){
+			console.log('Такого не может быть!');
+		} else if (time >= 0 && time < 8) {
+			console.log('Еще слишком рано!');
+			} else if (time >= 8 && time <=20) {
+				console.log('Время работать!');
+				open = true;
+				} else if (time > 20 && time <= 24) {
+					console.log('Уже слишком поздно!');
+					} else if (time > 24) {
+						console.log('В сутках только 24 часа!');
+					}
+	},
+	calcBudget: function calcBudget(){
+		alert("Бюджет на 1 день составляет: " + (mainList.budget / 30));
+	},
+	makeDiscount: function makeDiscount() {
+		if(mainList.discount)
+			price *= 0.8;
 	}
 }
 
-chooseGoods();
+start();
+mainList.chooseGoods();
+mainList.chooseShopItems();
+mainList.showUCanBuy();
+mainList.hireEmployers();
+mainList.workTime(12);
+mainList.calcBudget();
+mainList.makeDiscount();
 
-function discountSystem() {
-	if(mainList.discount)
-		return price * 0,8;
-	else
-		return price;
+for (let key in mainList){
+	console.log("Наш магазин включает в себя: " + key + " со значением " + mainList[key]);
 }
-
-function workTime(time){
-	if(time < 0){
-		console.log('Такого не может быть!');
-	} else if (time >= 0 && time < 8) {
-		console.log('Еще слишком рано!');
-		} else if (time >= 8 && time <=20) {
-			console.log('Время работать!')
-			} else if (time > 20 && time <= 24) {
-				console.log('Уже слишком поздно!');
-				} else if (time > 24) {
-					console.log('В сутках только 24 часа!');
-				}
-}
-workTime(18);
-
-function calcBudget(){
-	alert("Бюджет на 1 день составляет: " + mainList.budget/30);
-}
-
-calcBudget();
 
 
 /*let i = 0; 
