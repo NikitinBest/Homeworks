a645ed72e157b0442cc1d0522a7508e068eed1ff
Nikita <nikita06012003@mail.ru>
2018-06-16 17:47:41 +0300

    7 урок 2 Основное задание

--- lesson7.1/script.js
+++ lesson7.1/script.js
@@ -11,7 +11,7 @@ let now = document.getElementById("now");
 function writeTime () {
 	let date = new Date (Date.now());
 	now.textContent = "Сейчас: " + addNull(date.getHours()) + ":" + addNull(date.getMinutes()) + ":" + addNull(date.getSeconds()) +
-	" " + addNull(date.getDate()) + "." + addNull(date.getMonth()) + "." + date.getFullYear();
+	" " + addNull(date.getDate()) + "." + addNull(date.getMonth()+1) + "." + date.getFullYear();
 }
 
 setInterval(writeTime, 10);
