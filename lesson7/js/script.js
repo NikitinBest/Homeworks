25865a92dd3fd6473ca2084c1f43871908c50459
Nikita <nikita06012003@mail.ru>
2018-06-17 09:04:11 +0300

    7-8 урок Основное задание

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+
+	//Табы
 	let tab = document.getElementsByClassName("info-header-tab"),
 		tabContent = document.getElementsByClassName("info-tabcontent"),
 		info = document.getElementsByClassName("info-header")[0]; //  Родитель для табов
@@ -31,4 +33,61 @@ window.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	});
+
+
+	//Таймер
+	let deadLine = '2018-06-18';
+
+	function getTimeRemaining(endtime){
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor( (t/1000) % 60),
+			minutes = Math.floor( (t/1000/60) % 60),
+			hours = Math.floor( (t/1000/60/60));
+
+			return{
+				"total": t,
+				"hours": hours,
+				"minutes": minutes,
+				"seconds": seconds
+			};
+	};
+	function setClock(id, endtime) { //id - id элемента в HTML'
+	
+		let timeInterval = setInterval(updateClock, 1000);
+
+		let timer = document.getElementById(id),
+			hours = timer.querySelector(".hours"),
+			minutes = timer.querySelector(".minutes"),
+			seconds = timer.querySelector(".seconds");
+
+			function updateClock(){
+
+				function addNull (num){
+					if(num/10>=1 || num < 0)
+						return num;
+					else
+						return "0" + num;
+				}
+
+				function update(){
+
+					hours.innerHTML = addNull(t.hours);	
+					minutes.innerHTML = addNull(t.minutes);
+					seconds.innerHTML = addNull(t.seconds);
+				}
+
+				let t = getTimeRemaining(endtime);
+				update();
+
+				if(t.total <= 0){
+					clearInterval(timeInterval);
+					t = getTimeRemaining(new Date);
+					update();
+				}
+			};
+
+			updateClock();
+	};
+
+	setClock("timer", deadLine);
 });
