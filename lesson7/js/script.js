253c982ad9cd2551da9cce915aa31b9f72bf04af
Nikita <nikita06012003@mail.ru>
2018-06-19 18:34:32 +0300

    7-12 урок главное задание

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -114,4 +114,60 @@ window.addEventListener("DOMContentLoaded", () => {
 		document.body.style.overflow = '';
 
 	});
+
+
+	//Slider
+	let slideIndex = 1;
+		slides = document.getElementsByClassName("slider-item");
+		prev = document.querySelector(".prev");
+		next = document.querySelector(".next");
+		dotsWrap = document.querySelector(".slider-dots");
+		dot = document.getElementsByClassName("dot");
+
+	showSlides(slideIndex);
+
+	function showSlides (n) {
+		if(n > slides.length){
+			slideIndex = 1;
+		};
+
+		if(n < 1){
+			slideIndex = slides.length;
+		};
+
+		for(let i = 0; i < slides.length; i++){
+			slides[i].style.display = "none";
+		};
+
+		for(let i = 0; i < dot.length; i++){
+			dot[i].classList.remove("dot-active");
+		};
+
+		slides[slideIndex - 1].style.display = "block";
+		dot[slideIndex - 1].classList.add("dot-active");
+	}
+
+	function plusSlides(n){
+		showSlides(slideIndex += n)
+	}
+
+	function currentSlide(n){
+		showSlides(slideIndex = n);
+	}
+
+	prev.addEventListener("click", () =>{
+		plusSlides(-1);
+	});
+
+	next.addEventListener("click", () =>{
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener("click", function(event){
+		let target = event.target;
+		for(let i = 0; i < dot.length; i++){
+			if(target.classList.contains("dot") && target == dot[i-1]);
+				currentSlide(i);
+		}
+	});
 });
