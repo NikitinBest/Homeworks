1168f2b801da21395aed2c628a54d773cf34c6d2
Nikita <nikita06012003@mail.ru>
2018-06-19 16:13:26 +0300

    10 урок главное задание

--- lesson10/script.js
+++ lesson10/script.js
@@ -9,12 +9,13 @@ class Options {
 		this.textAlign = textAlign;
 	}
 
-	create () {
-		var div = document.createElement("div");
-		div.textContent = "Привет!";
-		div.style.cssText = `width: ${this.width}; height: ${this.height}; background: ${this.background}; font-size: ${this.fontSize}; text-align: ${this.textAlign}; `;
+	create (text) {
+		let div = document.body.appendChild(document.createElement("div"));
+		div.textContent = text;
+		div.style.cssText = `width: ${this.width}; height: ${this.height}; background: ${this.bg};
+			 font-size: ${this.fontSize}; text-align: ${this.textAlign};`;
 	}
 }
 
-let opt = new Options("140px", "120px", "green", "200%", "center");
-opt.create();
+let opt = new Options("140px", "500px", "green", "200%", "center");
+opt.create("Привет, мир!");
