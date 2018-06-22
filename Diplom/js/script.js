48ef25c6ada56143eab5969e5c736c14b7cf5f0b
Nikita <nikita06012003@mail.ru>
2018-06-22 13:51:51 +0300

    Дипломная работа

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -146,10 +146,8 @@ window.addEventListener("DOMContentLoaded", () => {
 			newViews = candidate[2].children[4],
 			newBio = candidate[2].children[5];
 
-		newImage.classList.remove("photo-1");
 		newImage.style.backgroundImage = `url("./img/construct-${slideIndex + sex*4}.png")`; //image
-		newImage.style.height = '195px';
-		newImage.style.width = '90px';
+		newImage.style.backgroundSize = 'contain';
 
 		newName.textContent = name.value;
 		newAge.textContent = age.value;
