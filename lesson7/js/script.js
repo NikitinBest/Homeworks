edd93ad44afeabc901acf4d5388deb4a416be322
Nikita <nikita06012003@mail.ru>
2018-06-24 17:26:08 +0300

    7-12 урок главное задание

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -112,7 +112,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		overlay.style.display = "none";
 		target.classList.remove("more-splash");
 		document.body.style.overflow = '';
-
+		statusMessage.innerHTML = "";
 	});
 
 
@@ -228,10 +228,88 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
 	//Form
-	let message = new new Object();
+	let message = new Object();
 	message.loading = "Загрузка...";
 	message.success = "Спасибо! Скоро мы свяжемся с вами";
 	message.failure = "Что-то пошло не так...";
 
-	
+	let form = document.getElementsByClassName("main-form")[0],
+		input = form.getElementsByTagName("input"),
+		statusMessage = document.createElement("div");
+		statusMessage.classList.add("status");
+
+		form.addEventListener("submit", function(event){
+			event.preventDefault();
+			form.appendChild(statusMessage);
+
+			//AJAX
+			let request = new XMLHttpRequest();
+			request.open("POST", "server.php");
+
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //Чтобы не было проболем с кодировкой
+
+			let formData = new FormData(form);
+
+			request.send(formData);
+
+			request.onreadystatechange = function (){
+				if(request.readyState < 4){
+					statusMessage.innerHTML = message.loading;					 
+				}else if(request.readyState === 4){
+					if(request.status == 200 && request.status < 300){
+						statusMessage.innerHTML = message.success;
+						// Добавляем контент на страницу
+					} 
+					else{
+						statusMessage.innerHTML = message.failure;
+					}			 
+				}
+			}
+
+			for(let i = 0; i < input.length; i++){
+				input[i].value = "";
+				//Очищаем поля ввода
+			}
+		});	
+
+
+	//emailForm
+	let EmailForm = document.getElementById("form"),
+		input1 = EmailForm.getElementsByTagName("input"),
+		statusMessage1 = document.createElement("div");
+		statusMessage1.classList.add("status");
+
+		EmailForm.addEventListener("submit", function(event){
+			event.preventDefault();
+			EmailForm.appendChild(statusMessage1);
+
+			//AJAX
+			let request = new XMLHttpRequest();
+			request.open("POST", "server.php");
+
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); //Чтобы не было проболем с кодировкой
+
+			let formData = new FormData(form);
+
+			request.send(formData);
+
+			request.onreadystatechange = function (){
+				if(request.readyState < 4){
+					statusMessage1.innerHTML = message.loading;					 
+				}else if(request.readyState === 4){
+					if(request.status == 200 && request.status < 300){
+						statusMessage1.innerHTML = message.success;
+						// Добавляем контент на страницу
+					} 
+					else{
+						statusMessage1.innerHTML = message.failure;
+					}			 
+				}
+			}
+
+			for(let i = 0; i < input1.length; i++){
+				input1[i].value = "";
+				//Очищаем поля ввода
+			}
+		});	
 });
