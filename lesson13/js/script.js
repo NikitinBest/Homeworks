5a6e0a84df87203623622b570e88153f83985921
Nikita <nikita06012003@mail.ru>
2018-06-25 10:46:29 +0300

    13 урок главное задание

--- lesson13/js/script.js
+++ lesson13/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	$(document).ready(function() {
-	    $(".main_btn:first, .main_btna:first, #sheldure").on("click", function(){
+	    $(".main_btn:first, .main_btna:first, a[href$=sheldure").on("click", function(){
 	            $(".overlay:first").fadeIn();
 	            $(".modal:first").slideDown();
 	    });
