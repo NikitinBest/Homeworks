36defd9bfdc91bf2ec395cd5e0b2dab3388a1aa2
Nikita <nikita06012003@mail.ru>
2018-06-25 10:10:01 +0300

    7-14 урок главное задание

--- lesson7/js/bundle/script.js
+++ lesson7/js/bundle/script.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-	let tab = require("./parts/tab.js");
-	let modal = require("./parts/modal.js");
-	let slider = require("./parts/slider.js");
-	let calc = require("./parts/calc.js");
-	let form = require("./parts/form.js");
-	let timer = require("./parts/timer.js");
+	let tab = require("../parts/tab.js");
+	let modal = require("../parts/modal.js");
+	let slider = require("../parts/slider.js");
+	let calc = require("../parts/calc.js");
+	let form = require("../parts/form.js");
+	let timer = require("../parts/timer.js");
 	
 	tab();
 	modal();
